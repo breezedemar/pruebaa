b6f8d025d98d1330600aaf634c6dd046ff9f568b
breezedemar <brissa.hernandez2021@gmail.com>
2022-06-30 22:02:26 -0500

    second commit

--- js/base.js
+++ js/base.js
@@ -1,11 +1,13 @@
-console.log("Hello JS")
+
+//console.log("Hello JS")
 
 //esto es un comentario 
 // esto es 
 
 //agregue un comentario despues de haber subido cambios a la rama 
 
-console.log("Hello JS")
-console.log("Hello JS")
-console.log("Hello JS")
-console.log("Hello JS")
+
+let gradosCelcius = prompt("Cuantos grados hay en tu ciudad")
+const F = (9/5 * gradosCelcius) + 32
+
+console.log(gradosCelcius+"grados celcius"+ "equivalen a "+ F +"grados celcius")
